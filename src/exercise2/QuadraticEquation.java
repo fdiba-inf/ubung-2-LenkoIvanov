34f5b201bbafdcf0984f3609b7235e3eb7096a5f
Lenko Ivanov <lenko.ivanov117@gmail.com>
2020-10-07 19:42:29 +0000

    ubung2

---
+++ src/exercise2/QuadraticEquation.java
@@ -0,0 +1,11 @@
+package exercise2;
+
+import java.util.Scanner;
+
+public class QuadraticEquation{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+
+    
+  }
+}
