85e3e4a310c2f42f89dfc665d80779785024926a
Lenko Ivanov <lenko.ivanov117@gmail.com>
2020-10-07 16:35:45 +0000

    ubung2

--- src/exercise2/PiApproximation.java
+++ src/exercise2/PiApproximation.java
@@ -6,7 +6,7 @@ public class PiApproximation{
     
      float pi = 4 * (1 - ((1/3) + (1/5)) - ((1/7) + (1/11)) - ((1/13) + (1/17)) - ((1/19) + (1/23)));
 
-     System.out.println("Pi: " + pi); 
+     System.out.println("Pi: " + pi); //how to converge to 3.14?
   }
 }
 
