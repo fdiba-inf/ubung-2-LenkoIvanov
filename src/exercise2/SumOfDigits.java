0c5036d8ed47e2f13d6e37064105129e36d57718
Lenko Ivanov <lenko.ivanov117@gmail.com>
2020-10-07 18:36:22 +0000

    ubung2

---
+++ src/exercise2/SumOfDigits.java
@@ -0,0 +1,28 @@
+package exercise2;
+
+import java.util.Scanner;
+
+public class SumOfDigits{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Enter a number from 0 to 999: ");
+    int number = input.nextInt();
+
+  
+    if(number < 0 || number > 999){
+      System.out.println("Invalid number!");
+    }
+    else{
+      int d1 = number % 10;
+      number = number/10;
+      int d2 = number % 10;
+      number = number/10;
+      int d3 = number % 10;
+
+      int sum = d1 + d2 + d3;
+      System.out.println("The sum of the digits of your number is " + sum);
+    }
+ 
+  }
+}
