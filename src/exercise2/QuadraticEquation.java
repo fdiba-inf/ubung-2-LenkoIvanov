2c95696f87d5e04fe607e087291729c551846298
Lenko Ivanov <lenko.ivanov117@gmail.com>
2020-10-08 05:46:16 +0000

    ubung2

--- src/exercise2/QuadraticEquation.java
+++ src/exercise2/QuadraticEquation.java
@@ -6,6 +6,41 @@ public class QuadraticEquation{
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
 
+    System.out.println("Enter integer values for a,b and c: ");
+    double a = input.nextDouble();
+    double b = input.nextDouble();
+    double c = input.nextDouble();
+
+
+    if(a != 0){
+      double d = (b * b) - (4.0 *a*c);
+      if(d > 0 || d == 0){
+       double x1 = (-b + Math.sqrt(d)) / (2.0 * a);
+       double x2 = (-b - Math.sqrt(d)) / (2.0 * a);
+
+       System.out.println(x1 + x2);
+      }
+      else{
+        System.out.println("Imaginary values");
+      }
+      
+    }
+    else {
+     if (b != 0){
+      double x = -b / (2*a);
+
+      System.out.println(x);
+     }
+
+      else{
+        if(c != 0){
+          System.out.println("No values");
+        }
+        else{
+          System.out.println("Many values");
+        }
+      }
+    }
+    }
     
   }
-}
